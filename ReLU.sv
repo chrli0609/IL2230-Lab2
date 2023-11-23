26b776edc3896171157134b8d01154c88c61a986
Christopher Liu <christopher.liu.0609@gmail.com>
2023-11-23 16:31:49 +0100

    updated files

--- ReLU.sv
+++ ReLU.sv
@@ -1,51 +1,8 @@
-module ReLU #(parameter N, parameter QM = 12, parameter QN = 20) (
+module ReLU #(parameter N = 2, parameter QM = 12, parameter QN = 20) (
+  input logic [QM + QN + N - 1:0] in,
+  output logic [7:0] out
 );
-
+  always_comb begin
+    out = in[QM + QN + N - 1] ? 0 : in;
+  end
 endmodule
-
-//48-bit to 8-bit ReLU LUT
-/*module relu_lut (
-    input logic signed [47:0] input_data,
-    output logic signed [7:0] output_data
-);
-
-    // Constants
-    localparam integer LUT_SIZE = 256; // Size of the lookup table
-    localparam integer LUT_BITS = 8;   // Number of bits for the lookup table index
-    localparam integer INPUT_FRACTIONAL_BITS = 30;
-    localparam integer INPUT_INTEGER_BITS = 18; // (48 - 30)
-
-    localparam integer OUTPUT_FRACTIONAL_BITS = 5;
-    localparam integer OUTPUT_INTEGER_BITS = 3; // (8 - 5)
-
-    // Lookup table
-    logic signed [7:0] lut [0:LUT_SIZE-1];
-
-    // Populate the lookup table with ReLU values
-    initial begin
-        integer i;
-        for (i = 0; i < LUT_SIZE; i++) begin
-            if (i - LUT_SIZE/2 <= 0)
-                lut[i] = 0;
-            else
-                lut[i] = (i - LUT_SIZE/2) * ((1 << OUTPUT_FRACTIONAL_BITS) - 1) / (LUT_SIZE/2);
-        end
-    end
-
-    // Apply the ReLU function using the lookup table
-    always_comb begin
-        integer index;
-        integer scaled_input;
-
-        scaled_input = input_data >> (INPUT_FRACTIONAL_BITS - LUT_BITS);
-        if (scaled_input < 0)
-            index = 0;
-        else if (scaled_input >= LUT_SIZE)
-            index = LUT_SIZE - 1;
-        else
-            index = scaled_input;
-
-        output_data = lut[index];
-    end
-endmodule
-*/
--- sigmoid.sv
+++ sigmoid.sv
@@ -1,4 +1,38 @@
-module FixedPointFunctions;
+module Sigmoid #(parameter N = 2, parameter QM = 12, parameter QN = 20) (
+    input logic [QM + QN + N - 1:0] in,
+    output logic [7:0] out
+);
+  // Define the size of the lookup table
+  parameter LUT_BITS = 7;
+  parameter LUT_SIZE = 2**LUT_BITS;
+  parameter LUT_QN = 4;
+  parameter LUT_OFFSET = (2.0**-LUT_QN) * LUT_SIZE / 2;
+
+  // Sigmoid lookup table for 8-bit fixed-point numbers 
+  logic signed [7:0] lut[LUT_SIZE];
+
+  // Initialize the sigmoid lookup table
+  initial begin
+    for (int i = 0; i < LUT_SIZE; i++) begin
+      // build lookup table for -4 to 4 with steps of 0.0625 when QN = 4
+      lut[i] = $signed($rtoi((2.0**LUT_QN) * (1.0 / (1.0 + $exp(-((i / (2.0**LUT_QN)) - LUT_OFFSET)))))); 
+    end
+  end
+  
+  always_comb begin
+    if (in[QM + QN + N - 1 : QN - LUT_QN] > LUT_OFFSET) begin
+      out = lut[LUT_SIZE];
+    end else if (in[QN + LUT_BITS - LUT_QN : QN - LUT_QN] > -LUT_OFFSET) begin
+      out = lut[in[QN + LUT_BITS - LUT_QN : QN - LUT_QN]];
+    end else begin
+      out = lut[0];
+    end
+  end
+
+endmodule
+
+/*
+module Sigmoid;
 
   // Define the size of the lookup table
   parameter LUT_SIZE = 128;
@@ -13,7 +47,7 @@ module FixedPointFunctions;
   // Initialize the sigmoid lookup table
   initial begin
     for (int i = 0; i < LUT_SIZE; i++) begin
-      lut[i] = $signed($rtoi(32.0 * (1.0 / (1.0 + exp(-((i / 32.0) - 4.0)))))); 
+      lut[i] = $signed($rtoi(16.0 * (1.0 / (1.0 + $exp(-(i / 16.0)))))); 
     end
   end
 
@@ -36,3 +70,4 @@ module FixedPointFunctions;
   endfunction
 
 endmodule
+*/
