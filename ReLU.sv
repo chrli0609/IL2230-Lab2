d35bace5b075560b582cc2f811ca386a5a2db306
Jonathan Lundstrom <git@jonl.us>
2023-11-24 12:04:12 +0100

    fixing inconsistent bit width and tests for relu

--- ReLU.sv
+++ ReLU.sv
@@ -1,8 +1,8 @@
 module ReLU #(parameter N = 2, parameter QM = 12, parameter QN = 20) (
-  input logic [QM + QN + N - 1:0] in,
-  output logic [7:0] out
+  input logic [QM + QN - 1:0] in,
+  output logic [QM + QN - 1:0] out
 );
   always_comb begin
-    out = in[QM + QN + N - 1] ? 0 : in;
+    out = in[QM + QN - 1] ? 0 : in;
   end
 endmodule
--- fully_parallel.sv
+++ fully_parallel.sv
@@ -8,20 +8,20 @@ module fully_parallel #(parameter N = 2, parameter QM = 12, parameter QN = 20, p
     input logic signed [31:0] in [N-1:0],
     input logic signed [15:0] weights [N-1:0],
     input logic bias,
-    output logic [7:0] out
+    output logic [QM + QN:0] out
 );
 
-logic [QM + QN + WM + WN + N - 1:0] mac_out [N-1:0];
+logic [QM + QN + WM + WN + N - 1:0] mac_out [N:0];
 logic no_overflow, no_underflow;
 logic [QM + QN + N - 1:0] mac_final;
-logic [7:0] out_not_registered;
+logic [QM + QN - 1:0] out_not_registered;
 
 //Fully parallel loop
 genvar i;
 generate
     //last iteration handles the bias
     for (i = 0; i <= N; i++) begin
-        if (i == N) MAC #(N, QM, QN, WM, WN, OB) mac (bias, 1, 0, mac_out[i]);
+        if (i == N) MAC #(N, QM, QN, WM, WN, OB) mac ({0,bias}, 1, mac_out[i-1], mac_out[i]);
         else if (i == 0) MAC #(N, QM, QN, WM, WN, OB) mac (in[i], weights[i], 0, mac_out[i]);
         else MAC #(N, QM, QN, WM, WN, OB) mac (in[i], weights[i], mac_out[i-1], mac_out[i]);
     end
@@ -29,10 +29,10 @@ generate
 endgenerate
 
 always_comb begin
-    no_overflow = ~|mac_out[N-1][QM + QN + WM + WN + N - 1 : QM + QN + WM + WN];   
-    no_underflow = &mac_out[N-1][QM + QN + WM + WN + N - 1 : QM + QN + WM + WN];
+    no_overflow = ~|mac_out[N][QM + QN + WM + WN + N - 1 : QM + QN + WM + WN];   
+    no_underflow = &mac_out[N][QM + QN + WM + WN + N - 1 : QM + QN + WM + WN];
     if (no_overflow | no_underflow) begin
-        mac_final = mac_out[N-1][QM + QN + WN - 1 : WN];
+        mac_final = mac_out[N][QM + QN + WN - 1 : WN];
     end else begin
         mac_final = $signed(mac_out[N-1][QM + QN + WM + WN + N - 1]);
     end
