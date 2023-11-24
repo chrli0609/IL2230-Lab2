bc12101e799382f9cb241b538f8132d3e0a81fda
Christopher Liu <christopher.liu.0609@gmail.com>
2023-11-24 16:04:06 +0100

    Added fully_serial.sv and its testbench

---
+++ fully_serial.sv
@@ -0,0 +1,119 @@
+`include "MAC.sv"
+`include "ReLU.sv"
+
+module fully_serial #(parameter N = 2, parameter QM = 12, parameter QN = 20, parameter WM = 6, parameter WN = 10, parameter OB = 8) (
+  input logic clk, rst_n,
+  input logic signed [QM + QN - 1:0] in [N-1:0],
+  input logic signed [WM + WN - 1:0] weights [N-1:0],
+  input logic bias,
+  output logic [QM + QN - 1:0] out,
+  output logic done
+);
+
+logic signed [QM + QN + WM + WN + N - 1 ] feedback_reg, feedback_next, mac_out;
+logic [$clog2(N): 0] i_reg, i_next;
+logic done_next;
+logic [QM + QN - 1:0] out_next;
+
+enum logic [1:0] {
+  MultAcc = 2'b00,
+  Bias = 2'b01,
+  ActFunc = 2'b10
+} curr_state, next_state;
+
+logic signed [QM + QN - 1:0] mac_in;
+logic signed [WM + WN - 1:0] mac_weight;
+
+logic no_overflow, no_underflow;
+logic [QM + QN + N - 1:0] mac_final;
+logic [QM + QN - 1:0] out_not_registered;
+
+MAC #(N, QM, QN, WM, WN, OB) mac (mac_in, mac_weight, feedback_reg, mac_out);
+
+
+ReLU #(N, QM, QN) my_func (mac_final, out_not_registered);
+
+
+//Next state logic
+always_comb begin
+  next_state = curr_state;
+  i_next = i_reg;
+  done_next = 0;
+
+  case (curr_state)
+    MultAcc: begin
+      if(i_reg == N) begin 
+        next_state = Bias;
+      end else begin 
+        next_state = MultAcc;
+        i_next = i_reg + 1;
+      end
+    end
+    Bias: begin
+      next_state = ActFunc;
+    end
+    ActFunc: begin
+      next_state = MultAcc;
+      i_next = 0;
+      done_next = 1;
+    end
+    default: begin
+      next_state = MultAcc;
+      i_next = 0;
+    end
+  endcase
+end
+
+always_comb begin
+    no_overflow = ~|feedback_reg[QM + QN + WM + WN + N - 1 : QM + QN + WM + WN];   
+    no_underflow = &feedback_reg[QM + QN + WM + WN + N - 1 : QM + QN + WM + WN];
+    if (no_overflow | no_underflow) begin
+        mac_final = feedback_reg[QM + QN + WN - 1 : WN];
+    end else begin
+        mac_final = $signed(feedback_reg[QM + QN + WM + WN + N - 1]);
+    end
+end
+
+always_comb begin
+  out_next = 0;
+    case (curr_state)
+      MultAcc: begin
+        mac_in = in[i_reg];
+        mac_weight = weights[i_reg];
+        
+        feedback_next = mac_out;
+      end
+      Bias: begin
+        mac_in = bias;
+        mac_weight = 1;
+      end
+      ActFunc: begin
+        feedback_next = 0;
+        out_next = out_not_registered;
+      end
+      default: begin
+        feedback_next = 0;
+      end
+  
+    endcase
+  end
+
+
+//State changing
+always_ff @(posedge clk, negedge rst_n) begin 
+  if(!rst_n) begin
+    curr_state <= MultAcc;
+    feedback_reg <= 0;
+    i_reg <= 0;
+    done <= 0;
+    out <= 0;
+  end else begin
+    curr_state <= next_state;
+    feedback_reg <= feedback_next;
+    i_reg <= i_next;
+    done <= done_next;
+    out <= out_next;
+  end
+end
+
+endmodule
---
+++ fully_serial_tb.sv
@@ -0,0 +1,104 @@
+//`include "fully_parallel.sv"  // Include the design file
+
+module fully_parallel_tb #(parameter N = 2, QM = 6, QN = 10, WM = 6, WN = 10, OB = 8);
+
+// Testbench signals
+logic clk;
+logic rst_n;
+logic signed [QM + QN - 1:0] in_data [N-1:0];
+logic signed [WM + WN - 1:0] weights [N-1:0];
+logic bias;
+logic [QM + QN - 1:0] out_data;
+
+localparam IN_SF = 2.0**-QN;
+localparam W_SF = 2.0**-WN;
+
+real mac_sum;
+real expected_out;
+logic [QM + QN - 1 :0] out_not_registered_tb;
+real mac_final_tb;
+logic [QM + QN + WM + WN + N - 1:0] mac_out_tb [N-1:0];
+
+
+
+
+assign out_not_registered_tb = dut.out_not_registered;
+assign mac_final_tb = dut.mac_final;
+assign mac_out_tb = dut.mac_out;
+
+// Instantiate the neuron module
+fully_parallel #(N, QM, QN, WM, WN, OB) dut (
+  .clk(clk),
+  .rst_n(rst_n),
+  .in(in_data),
+  .weights(weights),
+  .bias(bias),
+  .out(out_data)
+);
+
+// Clock generation
+initial begin
+  clk = 0;
+  forever #10 clk = ~clk;
+end
+
+initial begin
+  $dumpfile("dump.vcd");
+  $dumpvars(1, fully_parallel_tb);
+end
+
+// Test procedure
+initial begin
+  // Initialize inputs
+  rst_n = 0;
+  #1; 
+  rst_n = 1;
+  bias = 0;
+
+  for (int i = 0; i < N; i++) begin
+    weights[i] = 1;
+    in_data[i] = 0;
+  end
+  
+end
+
+
+always @(posedge clk, negedge rst_n) begin
+    
+    bias = $random;
+    for (int i = 0; i < N; i++) begin
+      weights[i] = $urandom_range(0,2 ** WM);
+      in_data[i] = $urandom_range(0,2 ** QM);
+      weights[i] = $signed(weights[i]) - 2 ** (WM - 1);
+      in_data[i] = $signed(in_data[i]) - 2 ** (QM - 1);
+      weights[i] = weights[i] << (WN - (WM/2));
+      in_data[i] = in_data[i] << (QN - (QM/2));
+    end
+
+    @(posedge clk)
+    mac_sum = 0.0;
+    for (int i = 0; i<N; i++) begin
+      mac_sum = mac_sum + ($itor(in_data[i]) * IN_SF * $itor(weights[i]) * W_SF);
+    end
+    
+    if (mac_sum <= 0) begin
+      expected_out = 0;
+    end else begin
+      expected_out = mac_sum;
+    end
+
+    assert(expected_out == ($itor(out_data) * IN_SF)) $display("TEST PASSED");
+    else $error("TEST FAILED\n",
+      "in[0]: %d\n", in_data[0],
+      "in[1]: %d\n", in_data[1],
+      "weights[0]: %d\n", weights[0],
+      "weights[1]: %d\n", weights[1],
+      "mac_sum: %d\n", mac_sum,
+      "expected_out: %f\n", expected_out,
+      "out_data: %f\n", $itor(out_data) * IN_SF
+      );
+
+end
+
+
+endmodule
